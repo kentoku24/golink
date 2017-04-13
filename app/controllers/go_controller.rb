9da2341240823863e976278b0fa6aa9ea3d12216
Kent <kentokumatsunami@gmail.com>
2017-04-13 23:33:41 +0900

    added 前方一致機能

--- app/controllers/go_controller.rb
+++ app/controllers/go_controller.rb
@@ -3,7 +3,7 @@ class GoController < ApplicationController
     word = params[:word]
     #this & notation is way too new for heroku, so sticking to old fasion way
     #url = Link.find_by(name: word)&.url
-    url = Link.find_by(name: word).try(:url)
+    url = Link.find_by(name: word).try(:url) || Link.where(Link.arel_table[:name].matches(word + "%")).limit(1).try!(:first).try!(:url)
     if url
       redirect_to url
     else
