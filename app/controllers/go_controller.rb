6458ce013731d2c3669b942a07d787aa486d3d20
Kent <kentokumatsunami@gmail.com>
2017-04-15 03:08:29 +0900

    now it takes arbitraly length URL

--- app/controllers/go_controller.rb
+++ app/controllers/go_controller.rb
@@ -1,13 +1,18 @@
 class GoController < ApplicationController
   def convert
-    word = params[:word]
+    word, rest = params[:other].split("/", 2) 
+    #word = params[:word]
     #this & notation is way too new for heroku, so sticking to old fasion way
     #url = Link.find_by(name: word)&.url
     url = Link.find_by(name: word).try(:url) || Link.where(Link.arel_table[:name].matches(word + "%")).limit(1).try!(:first).try!(:url)
     if url
-      redirect_to url
+      if rest
+        redirect_to url + "/" + rest
+      else
+        redirect_to url
+      end
     else
-      redirect_to controller: 'links'
+      redirect_to controller: 'links', notice: params
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :links
-  get ':word', to: 'go#convert'
+  #get ':word', to: 'go#convert'
+
+  get '*other', to: 'go#convert'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
