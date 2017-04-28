6e8780d67f28ae089d39c92fb8bcd5e4058516e6
Kent <kentokumatsunami@gmail.com>
2017-04-29 07:28:01 +0900

    small refactoring

--- app/controllers/suggest_controller.rb
+++ app/controllers/suggest_controller.rb
@@ -1,8 +1,8 @@
 class SuggestController < ApplicationController
   def search
     puts params[:search_term]
-    links = Link.where(Link.arel_table[:name].matches(params[:search_term] + "%"))
-    puts links
+    links = Link.where(Link.arel_table[:name].matches(params[:search_term] + '%'))
+
     names, urls = links.reduce([[],[]]) { |memo, elem| 
           memo[0].push(elem.name)
           memo[1].push(elem.url)
