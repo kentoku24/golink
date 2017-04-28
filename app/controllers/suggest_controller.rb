7412f5c01b501ae529978cab668368a016221c77
Kent <kentokumatsunami@gmail.com>
2017-04-28 22:30:42 +0900

    implemented suggest

--- app/controllers/suggest_controller.rb
+++ app/controllers/suggest_controller.rb
@@ -1,7 +1,15 @@
 class SuggestController < ApplicationController
   def search
     puts params[:search_term]
-    render json: ["foo", ["foo","bar"]]
+    links = Link.where(Link.arel_table[:name].matches(params[:search_term] + "%"))
+    puts links
+    names, urls = links.reduce([[],[]]) { |memo, elem| 
+          memo[0].push(elem.name)
+          memo[1].push(elem.url)
+          memo
+        }
+
+    render json: [params[:search_term], names, urls]
   end
 
 end
