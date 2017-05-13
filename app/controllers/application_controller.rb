386d5f0d0a70b783371972c7d9c44b59b411135a
Kent <kentokumatsunami@gmail.com>
2017-05-13 12:19:31 +0900

    added access control

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
+  before_action :block_foreign_hosts
   protect_from_forgery with: :exception
+end
+
+def block_foreign_hosts
+  whitelist_prefix = ENV['whitelist_prefix'].split(',')
+  
+  logger.info(whitelist_prefix + " remote_ip: " + request.remote_ip)
+  whitelist_prefix.each do |item|
+    return false if request.remote_ip.start_with?(item)
+  end
+  raise ActionController::RoutingError.new('Not Found')
 end
